fa50fdbdd9bc11f8665608bd37ed7fa7fd58b5cf
Amiti Uttarwar <amiti@uttarwar.org>
2020-06-02 11:43:57 -0700

    [doc] Describe different connection types

--- src/net.h
+++ src/net.h
@@ -114,12 +114,12 @@ struct CSerializedNetMsg
 };
 
 enum class ConnectionType {
-    INBOUND,
-    OUTBOUND,
-    MANUAL,
-    FEELER,
-    BLOCK_RELAY,
-    SCOUT,
+    INBOUND, // peer initiated connections
+    OUTBOUND, // full relay connections (blocks, addrs, txns)
+    MANUAL, // connections to addresses added via addnode or the connect command line argument
+    FEELER, // short lived connections used to test address validity
+    BLOCK_RELAY, // only relay blocks to these connections
+    SCOUT, // short lived connections used to solicit addrs
 };
 
 class NetEventsInterface;
